8bc13d0799fc17e604fd0d76412781f23c8674a7
wanloparu <wanlop.aru@rmutr.ac.th>
2022-07-08 11:38:08 +0700

    update file

---
+++ hello.py
@@ -0,0 +1,51 @@
+print("Hellos")
+
+"""
+คำสั่งนี้ใช้ในการแสดงผลข้อความ
+และรองแสดงได้ทุกชนิด
+นะครับทุกคน
+"""
+print("Hello World 555") 
+
+#string => ข้อความ ตัวอักษร ' "" '''
+#number => integer , float
+#boolean => true , false
+print(-1)
+print(0)
+print("1"+"9")
+print(3.99)
+print(True)
+print(False)
+print("kongruksiam \n" + "studio")
+
+
+# Data Type & Variable
+# ชื่อตัวแปร = ค่าที่เก็บในตัวแปร
+
+x = 20 #integer
+y = 3.99
+z = True
+# print("ผลลัพธ์ = "+str(x))
+# print(y)
+# print(z)
+print(type(y))
+print(type("kongruksiam"))
+print(type(z))
+
+
+#กฎการตั้งชื่อ
+
+"""
+1.ใส่ตัวเลข ตัวอักษร เครื่องหมาย 
+2.ห้ามขึ้นต้นด้วยตัวเลขและสัญสักษณ์ ยกเว้น _ 
+3.ห้ามซ้ำกับ keywords
+4.case sensitive 
+"""
+
+name="A"
+NAME="B"
+nAmE="C"
+
+print(name)
+print(NAME)
+print(nAmE)
